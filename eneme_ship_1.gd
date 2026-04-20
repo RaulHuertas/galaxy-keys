4c1b37663532ffde2d1e0a67041952d57719297e
Raul Huertas <rax20037@gmail.com>
2026-04-20 13:56:11 -0500

    improving the enemies

--- eneme_ship_1.gd
+++ eneme_ship_1.gd
@@ -3,13 +3,12 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(%nuty_ship)
-	%nuty_ship.play()
+	%sprite.play()
 	
-	#%enemy_ship.hide()
-	pass # Replace with function body.
-
-
+	
+func kill():
+	%sprite.hide()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
