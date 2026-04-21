32cc598e32f2f80eda28845148f1a2d8c56c6dce
Raul Huertas <rax20037@gmail.com>
2026-04-21 17:30:13 -0500

    adding instancing

--- practice_game.gd
+++ practice_game.gd
@@ -6,7 +6,7 @@ var enemy_ships : Array = []
 var current_ship : EnemyShip = null
 # Called when the node enters the scene tree for the first time.
 const characters_level_1 = ["f","g","h","j"]
-@onready var main_chip = %nuty_ship
+@onready var main_ship = %main_ship
 const character_levels : Array = [characters_level_1]
 
 enum State {
@@ -21,23 +21,25 @@ func get_target_array(level: int =0, limit: int = 1)->String:
 	for i in limit:
 		var random_index : int = randi()%character_levels[level].size()
 		result = result+(character_levels[level][random_index])
+
 	return result
 
-func _ready():
-	print(%nuty_ship)
-	%nuty_ship.play()
-	state = State.FREE		
-	%enemy_ship_1.hide()
-	
-	#Initialize enemies
-	#enemy_ships.append(%enemy_ship_1)
+func spawn_enemies():
+	enemy_ships.clear()
 	var viewport_size = get_viewport_rect().size
-	var right_x_min = viewport_size.x * 0.60
-	var right_x_max = viewport_size.x - 50
+	var radius = min(viewport_size.x, viewport_size.y)/2.0
+	var max_radius = radius*0.95
+	var min_radius = radius*0.7
+	
+	
 	for enemy in character_levels[current_level].size():
 		var new_ship = enemy_ship_scene_1.instantiate()
-		new_ship.position.x = randf_range(right_x_min, right_x_max)
-		new_ship.position.y = randf_range(50, viewport_size.y - 50)
+		var random_angle = randf_range(0.0, 2.0*PI)
+		var random_radius = randf_range(min_radius, max_radius)
+		#new_ship.position.x = viewport_size.x*0.5+viewport_size.x*0.5*cos(random_angle)
+		#new_ship.position.y = viewport_size.y*0.5+viewport_size.y*0.5*sin(random_angle)
+		new_ship.position.x = viewport_size.x*0.5+random_radius*cos(random_angle)
+		new_ship.position.y = viewport_size.y*0.5+random_radius*sin(random_angle)
 		self.add_child(new_ship)
 		enemy_ships.append(new_ship)
 		new_ship.show()
@@ -45,7 +47,21 @@ func _ready():
 	
 	for i in enemy_ships.size():
 		var ship = enemy_ships[i]
-		ship.set_assignations(get_target_array(current_level,1),get_target_array(current_level,5))
+		ship.set_assignations(
+			character_levels[current_level][i],
+			get_target_array(current_level,5)
+		)
+		
+func _ready():
+	print(%nuty_ship)
+	main_ship.play()
+	state = State.FREE		
+	%enemy_ship_1.hide()
+	var viewport_size = get_viewport_rect().size
+	main_ship.position.x = viewport_size.x/2
+	main_ship.position.y = viewport_size.y/2
+	#Initialize enemies
+	spawn_enemies()
 	
 func aim_to_ship(ship_to_rotate:Node2D, target : Node2D):
 	var direction = target.global_position - ship_to_rotate.global_position
@@ -55,18 +71,24 @@ func aim_to_ship(ship_to_rotate:Node2D, target : Node2D):
 func _process(delta):
 	#make the main ship aim it's locked enemy
 	if state == State.LOCKED:
-		aim_to_ship(%nuty_ship, %enemy_ship_1.get_sprite())
+		aim_to_ship(main_ship, current_ship.get_sprite())
 	else:
-		%nuty_ship.rotation = %nuty_ship.rotation+0.5*delta
+		main_ship.rotation = main_ship.rotation+0.5*delta
 		
 	for i in enemy_ships.size():
 			var ship = enemy_ships[i]
-			aim_to_ship(ship, main_chip)
+			aim_to_ship(ship.get_sprite(), main_ship)
 	
-	#make the enemy shis aim the main ship
-	aim_to_ship(%enemy_ship_1.get_sprite(), %nuty_ship)
 	pass
 
+func remaining_ships()->int:
+	var ret : int = 0
+	for ship in enemy_ships.size():
+		if enemy_ships[ship].is_destroyed():
+			ret = ret+1
+	
+	return enemy_ships.size()-ret
+	
 func _input(event):
 	if event is InputEventKey and event.pressed and not event.echo:
 		var key = OS.get_keycode_string(event.keycode).to_lower()
@@ -83,6 +105,8 @@ func _input(event):
 			if current_ship.is_destroyed():
 				print("Hit and destroyed!")
 				state = State.FREE
+				if remaining_ships()==0:
+					spawn_enemies()
 			pass
 			
 			
